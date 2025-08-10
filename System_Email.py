c29e4a596aee884bcfe46d622de8c304208348f7
unyime101 <unyimesmith@gmail.com>
2025-08-11 00:54:51 +0100

    .env added.

--- System_Email.py
+++ System_Email.py
@@ -1,10 +1,7 @@
 import smtlib, ssl
+from .env import my_email, password, target_email
 port = 465 # for ssl
 smtp_server = "smtp.gmail.com"
-sender_email = "oomspi5@gmail.com"
-receiver_email = "unyimesmith@gmail.com"
-password =dmxw qgoi riec umim
-
 def send_alert(message):
   context =ssl.create_default_context()
 
