5bf849762bb6643fb2a48d7ec5635e85b8a5a1b4
unyime101 <unyimesmith@gmail.com>
2025-08-10 23:45:57 +0100

    Mad

---
+++ System_Email.py
@@ -0,0 +1,11 @@
+import smtlib, ssl
+port = 465 # for ssl
+smtp_server = "smtp.gmail.com"
+sender_email = "oomspi5@gmail.com"
+receiver_email = "unyimesmith@gmail.com"
+password =dmxw qgoi riec umim
+
+def send_alert(message):
+  context =ssl.create_default_context()
+
+with 
