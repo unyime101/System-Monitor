7325d1659602bfff69bc5827d63c5462a5b9b1a7
unyime101 <unyimesmith@gmail.com>
2025-08-11 01:00:27 +0100

    changing stuctures

--- System_Email.py
+++
@@ -1,8 +0,0 @@
-import smtlib, ssl
-from .env import my_email, password, target_email
-port = 465 # for ssl
-smtp_server = "smtp.gmail.com"
-def send_alert(message):
-  context =ssl.create_default_context()
-
-with   g
